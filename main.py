2e24ad8e855aa311eac26c5fbe9ce14e742dbaeb
DELL <wassim.rifay@uit.ac.ma>
2023-12-19 12:06:30 +0100

    delete some spaces

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, conlist
 from typing import Dict, List, Optional
 import pandas as pd
 from recipes_suggestion import generate_recipes_suggestions
-from repas_suggestions   import generate_repas_programme
+from repas_suggestions import generate_repas_programme
 from Person import Person
 
 dataset = pd.read_csv('./Data/dataset.csv', compression='gzip')
@@ -15,54 +15,61 @@ class params(BaseModel):
     n_neighbors: int = 5
     return_distance: bool = False
 
+
 class Recipe(BaseModel):
-    Recipe_Name:str
-    Recipe_Image_link:str
-    Recipe_nutritions_values:conlist(float, min_items=9, max_items=9)
-    RecipeIngredient:List[str]
-    RecipeInstructions:List[str]
-    CookTime:str
-    PrepTime:str
-    TotalTime:str
+    Recipe_Name: str
+    Recipe_Image_link: str
+    Recipe_nutritions_values: conlist(float, min_items=9, max_items=9)
+    RecipeIngredient: List[str]
+    RecipeInstructions: List[str]
+    CookTime: str
+    PrepTime: str
+    TotalTime: str
 
 
 class RecipePredictionIn(BaseModel):
-    nutrition_input:conlist(float, min_items=9, max_items=9)
-    number_of_recommendations:int
-    ingredients:List[str] = []
+    nutrition_input: conlist(float, min_items=9, max_items=9)
+    number_of_recommendations: int
+    ingredients: List[str] = []
+
 
 class RecipePredictionOut(BaseModel):
     Message: str
     output: Optional[List[Recipe]] = None
 
+
 class RepasPredictionIn(BaseModel):
-    age:int
-    height:int
-    weight:int
-    gender:str
-    activity:str
-    number_of_meals:int
-    weight_loss_plan:str
-    nutrition_input:conlist(float, min_items=9, max_items=9)
-    ingredients:List[str] = []
-    params:Optional[params]
+    age: int
+    height: int
+    weight: int
+    gender: str
+    activity: str
+    number_of_meals: int
+    weight_loss_plan: str
+    nutrition_input: conlist(float, min_items=9, max_items=9)
+    ingredients: List[str] = []
+    params: Optional[params]
+
 
 class NutritionProgramme(BaseModel):
-    BMI:str
-    BMICategory:str
-    CaloriesPerDay:str
-    Repas_Programme:Dict[str, List[Recipe]]
+    BMI: str
+    BMICategory: str
+    CaloriesPerDay: str
+    Repas_Programme: Dict[str, List[Recipe]]
+
 
 class RepasPredictionOut(BaseModel):
     Message: str
     output: Optional[NutritionProgramme] = None
 
+
 @app.get("/")
 def home():
     return {"Food Recommendation System": "OK"}
 
-@app.post("/Recipe_suggestions/",response_model=RecipePredictionOut)
-def predict_recipes(prediction_input:RecipePredictionIn):
+
+@app.post("/Recipe_suggestions/", response_model=RecipePredictionOut)
+def predict_recipes(prediction_input: RecipePredictionIn):
     output = generate_recipes_suggestions(
         dataset,
         RecipePredictionIn.nutrition_list,
@@ -73,15 +80,16 @@ def predict_recipes(prediction_input:RecipePredictionIn):
         return {
             "Message": "Not found",
             "output": None
-            }
+        }
     else:
         return {
-            "Message" : "Get recipes successfully",
+            "Message": "Get recipes successfully",
             "output": output
-            }
+        }
 
-@app.post("/Repas_suggestions/",response_model=RepasPredictionOut)
-def predict_repas(prediction_input:RepasPredictionIn):
+
+@app.post("/Repas_suggestions/", response_model=RepasPredictionOut)
+def predict_repas(prediction_input: RepasPredictionIn):
     age = RepasPredictionIn.age
     height = RepasPredictionIn.height
     weight = RepasPredictionIn.weight
@@ -91,24 +99,24 @@ def predict_repas(prediction_input:RepasPredictionIn):
 
     if number_of_meals == 3:
         meals_calories_perc = {
-            'breakfast':0.35,
-            'lunch':0.40,
-            'dinner':0.25
+            'breakfast': 0.35,
+            'lunch': 0.40,
+            'dinner': 0.25
         }
     elif number_of_meals == 4:
         meals_calories_perc = {
-            'breakfast':0.30,
-            'morning snack':0.05,
-            'lunch':0.40,
-            'dinner':0.25
+            'breakfast': 0.30,
+            'morning snack': 0.05,
+            'lunch': 0.40,
+            'dinner': 0.25
         }
     else:
-        meals_calories_perc={
-            'breakfast':0.30,
-            'morning snack':0.05,
-            'lunch':0.40,
-            'afternoon snack':0.05,
-            'dinner':0.20
+        meals_calories_perc = {
+            'breakfast': 0.30,
+            'morning snack': 0.05,
+            'lunch': 0.40,
+            'afternoon snack': 0.05,
+            'dinner': 0.20
         }
 
     weight_loss_plan = RepasPredictionIn.weight_loss_plan
@@ -129,10 +137,9 @@ def predict_repas(prediction_input:RepasPredictionIn):
         return {
             "Message": "Not found",
             "output": None
-            }
+        }
     else:
         return {
-            "Message" : "Get recipes successfully",
+            "Message": "Get recipes successfully",
             "output": output
-            }
-
+        }
