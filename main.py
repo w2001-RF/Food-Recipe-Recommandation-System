0272a5f01ccd0c151a0031b9b3895271e68d7738
DELL <wassim.rifay@uit.ac.ma>
2023-12-19 10:31:36 +0100

    add params class

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ dataset=pd.read_csv('./Data/dataset.csv',compression='gzip')
 
 app = FastAPI()
 
+
+class params(BaseModel):
+    n_neighbors: int = 5
+    return_distance: bool = False
+
+
+
 @app.get("/")
 def home():
     return {"Food Recommendation System": "OK"}
