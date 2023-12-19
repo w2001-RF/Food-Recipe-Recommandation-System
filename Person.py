b23ec3e46a736fb10b1c9b784dc21dfc9e7e69e9
DELL <wassim.rifay@uit.ac.ma>
2023-12-19 10:54:21 +0100

    add get_bmi_string_and_category method to Person class

--- Person.py
+++ Person.py
@@ -12,5 +12,20 @@ class Person:
         bmi=round(self.weight/((self.height/100)**2),2)
         return bmi
 
+    def get_bmi_string_and_category(self,):
+        bmi=self.calculate_bmi()
+        bmi_string=f'{bmi} kg/m²'
+        if bmi<18.5:
+            category='Underweight'
+        elif 18.5<=bmi<25:
+            category='Normal'
+        elif 25<=bmi<30:
+            category='Overweight'
+        else:
+            category='Obesity'
+        return bmi_string,category
+
+    
+
 
 
