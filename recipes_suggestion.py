e0a8b125ced9277b5f7e1e83ffe7d9b2216e1935
DELL <wassim.rifay@uit.ac.ma>
2023-12-19 11:24:20 +0100

    add nutrition_values

--- recipes_suggestion.py
+++ recipes_suggestion.py
@@ -1,22 +1,30 @@
 from model import generate
 from ImageFinder.ImageFinder import get_images_links as find_image
 
+nutrition_values = [
+    'Calories','FatContent','SaturatedFatContent',
+    'CholesterolContent','SodiumContent','CarbohydrateContent',
+    'FiberContent','SugarContent','ProteinContent'
+]
+
 class Recommendation:
-    def __init__(self,dataframe, nutrition_list,nb_recommendations,ingredient_txt):
+    def __init__(self,dataframe, nutrition_list,nb_recommendations,ingredients):
         self.nutrition_list=nutrition_list
         self.nb_recommendations=nb_recommendations
-        self.ingredient_txt=ingredient_txt
+        self.ingredients=ingredients
         self.dataframe = dataframe
         pass
 
     def generate(self,):
         params={'n_neighbors':self.nb_recommendations,'return_distance':False}
-        ingredients=self.ingredient_txt.split(';')
         recommendations = generate(self.dataframe,
-                                   self.nutrition_list,ingredients,
+                                   self.nutrition_list,
+                                   self.ingredients,
                                    params)
 
         if recommendations!=None:
             for recipe in recommendations:
                 recipe['image_link']=find_image(recipe['Name'])
         return recommendations
+
+
