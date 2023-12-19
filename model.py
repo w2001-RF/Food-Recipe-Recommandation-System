11565a6d35285432fea588fb0f3d74aa6e35d754
DELL <wassim.rifay@uit.ac.ma>
2023-12-19 10:43:59 +0100

    add function to extract data filtered by ingredients

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
@@ -8,11 +9,9 @@ def scaling(dataframe):
     prep_data = scaler.fit_transform(dataframe.iloc[:, 6:15].to_numpy())
     return prep_data, scaler
 
-
 def build_pipeline(neigh, scaler, params):
     transformer = FunctionTransformer(neigh.kneighbors, kw_args=params)
     pipeline = Pipeline([('std_scaler', scaler), ('NN', transformer)])
     return pipeline
-
 
 
