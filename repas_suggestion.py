9888be28d72cf84d2a4f9c46fdadf7c04d64d44d
DELL <wassim.rifay@uit.ac.ma>
2023-12-19 11:49:02 +0100

    add repas_suggestions file

---
+++ repas_suggestion.py
@@ -0,0 +1,14 @@
+Weights = {
+    "Maintain weight": 1,
+    "Mild weight loss": 0.9,
+    "Weight loss": 0.8,
+    "Extreme weight loss": 0.6
+}
+nutritions_values=[
+    'Calories','FatContent','SaturatedFatContent',
+    'CholesterolContent','SodiumContent','CarbohydrateContent',
+    'FiberContent','SugarContent','ProteinContent'
+]
+losses=['-0 kg/week','-0.25 kg/week','-0.5 kg/week','-1 kg/week']
+
+
