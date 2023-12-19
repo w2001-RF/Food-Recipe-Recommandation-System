53ca99a4d3334749e2dc44e9f93fe70cc6c8a59c
DELL <wassim.rifay@uit.ac.ma>
2023-12-19 11:00:32 +0100

    add Recommendation class to recipes_suggestions.py

---
+++ recipes_suggestion.py
@@ -0,0 +1,8 @@
+
+
+class Recommendation:
+    def __init__(self,nutrition_list,nb_recommendations,ingredient_txt):
+        self.nutrition_list=nutrition_list
+        self.nb_recommendations=nb_recommendations
+        self.ingredient_txt=ingredient_txt
+        pass
