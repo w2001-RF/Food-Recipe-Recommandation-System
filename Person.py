1222a1b0befafcf41b356c0adc3a98f5896d18db
DELL <wassim.rifay@uit.ac.ma>
2023-12-19 10:53:04 +0100

    add Person class

---
+++ Person.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self,age,height,weight,gender,activity,meals_calories_perc,weight_loss_plan):
+        self.age=age
+        self.height=height
+        self.weight=weight
+        self.gender=gender
+        self.activity=activity
+        self.meals_calories_perc=meals_calories_perc
+        self.weight_loss_plan=weight_loss_plan
+
+    
+
